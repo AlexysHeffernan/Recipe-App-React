107d19076f85e50c4d911ebdf2af5fb19bb6952f
Alexys Heffernan <160342586+AlexysHeffernan@users.noreply.github.com>
2026-05-22 20:09:14 -0400

    Format ingredients and preparation with newlines
    Updated ingredient and preparation strings to use newline characters for better formatting.

--- src/RecipeData.js
+++ src/RecipeData.js
@@ -8,8 +8,8 @@ const RecipeData = [
       name: "Tuna Poke with Mango",
       cuisine: "Hawaiian",
       photo: "https://www.foodista.com/sites/default/files/styles/recype/public/tunapokebowlwithmango22.jpg",
-      ingredients: "1 package of sushi grade tuna. 1 cup cooked quinoa ½ avocado, sliced. ½ mango, cubed. 1 shredded carrot. 1 small sliced cucumber. poke sauce.",
-      preparation: "1. Chop tuna into cubes. 2. Toss with 1 tbsp sesame oil and 1 tbsp tamari. Set aside. 3. Layer your poke bowl starting with quinoa. 4. Whisk all poke sauce ingredients in a bowl and pour over the poke bowl. 5. Garnish with sesame seeds and furikake. 6. Top with chopped scallions."
+      ingredients: "1 package of sushi grade tuna. /n1 cup cooked quinoa. /n½ avocado, sliced. /n½ mango, cubed. /n1 shredded carrot. /n1 small sliced cucumber. /npoke sauce.",
+      preparation: "1. Chop tuna into cubes. /n2. Toss with 1 tbsp sesame oil and 1 tbsp tamari. Set aside. /n3. Layer your poke bowl starting with quinoa. /n4. Whisk all poke sauce ingredients in a bowl and pour over the poke bowl. /n5. Garnish with sesame seeds and furikake. /n6. Top with chopped scallions."
     },
     {
       name: "Guacamole",
